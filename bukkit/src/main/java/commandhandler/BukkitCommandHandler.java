3228018d4b1ba5eae7191d53f27bb4250d170f1a
andris <andris@ANDRIS>
2024-03-18 17:00:55 +0100

    Implemented commands skeleton

--- bukkit/src/main/java/commandhandler/BukkitCommandHandler.java
+++ bukkit/src/main/java/commandhandler/BukkitCommandHandler.java
@@ -4,7 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.List;
 
 public class BukkitCommandHandler implements TabExecutor {
@@ -15,7 +14,7 @@ public class BukkitCommandHandler implements TabExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand( CommandSender sender,  Command command,  String label, String[] args) {
         if (command.getName().equalsIgnoreCase("sapm")) {
             return commandHandler.handleCommand(sender, label, args);
         }
@@ -23,7 +22,7 @@ public class BukkitCommandHandler implements TabExecutor {
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete( CommandSender sender,  Command command, String alias,  String[] args) {
         if (command.getName().equalsIgnoreCase("sapm")) {
             return commandHandler.handleTabComplete(sender, alias, args);
         }
